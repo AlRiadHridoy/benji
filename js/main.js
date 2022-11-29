8720cf2b97b5ddcf8e49964d20aff51a9185eb2c
Al Riad Hridoy <64416616+AlRiadHridoy@users.noreply.github.com>
2022-11-29 09:30:20 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  $(".hamburger").click(function () {
+    $("ul").toggleClass("toggle");
+  });
+  $("nav ul a").click(function () {
+    $("ul").toggleClass("toggle");
+  });
+
+  $('a[href^="#"]').click(function () {
+    console.log($('a[href^="#"]'));
+    var hash = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 64,
+      },
+      600
+    );
+    return false;
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 58) {
+      $("header").addClass("sticky");
+    } else {
+      $("header").removeClass("sticky");
+    }
+  });
+
+  if ($(window).scrollTop() > 58) {
+    $("header").addClass("sticky");
+  }
+});
+
+// scroll window
+$(window)
+  .scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    // Show/hide menu on scroll
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
+
+    // Assign active class to nav links while scolling
+    $("section").each(function (i) {
+      if ($(this).position().top - 100 <= scrollDistance) {
+        $(".navigation li a.active").removeClass("active");
+        $(".navigation li a").eq(i).addClass("active");
+      }
+    });
+  })
+  .scroll();
